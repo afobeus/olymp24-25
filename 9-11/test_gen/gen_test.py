eb0137d6ddeee82883b4e33f8590ea89f513129e
Artem <artem.dzichkanets@mail.ru>
2024-11-17 23:23:10 +0300

    gen_test.py and a_test added

--- 9-11/test_gen/gen_test.py
+++ 9-11/test_gen/gen_test.py
@@ -1,9 +1,10 @@
 import sys
 import os
+import shutil
 from pathlib import Path
 
 CURRENT_TASK = 'a'
-COMPILER_PATH = "C:/MinGW/bin"
+COMPILER_PATH = "C:/mingw64/bin"
 
 if CURRENT_TASK == 'a':
     from a_test import TEST_CASES, get_test_string
@@ -13,13 +14,15 @@ test_count = 1
 path = Path(os.getcwd())
 parent_path = str(path.parent.absolute()).replace('\\', '/')
 os.chdir(parent_path)
+shutil.rmtree(f"tests/{CURRENT_TASK}")
+os.mkdir(f"tests/{CURRENT_TASK}")
 for case, count in TEST_CASES:
     for i in range(count):
-        with open(fr"tests/{test_count}", 'w') as file:
+        with open(fr"tests/{CURRENT_TASK}/{test_count}", 'w') as file:
             file.write(get_test_string(*case.gen_case_common()))
 
-        os.system(f"{COMPILER_PATH}/task_{CURRENT_TASK}.exe < tests/{test_count} > temp_cpp.a")
-        os.system(f"python solutions/sol_{CURRENT_TASK}.py < tests/{test_count} > temp_py.a")
+        os.system(f"{COMPILER_PATH}/task_{CURRENT_TASK}.exe < tests/{CURRENT_TASK}/{test_count} > temp_cpp.a")
+        os.system(f"python solutions/sol_{CURRENT_TASK}.py < tests/{CURRENT_TASK}/{test_count} > temp_py.a")
 
         with open("temp_cpp.a") as file:
             cpp_ans = file.read()
@@ -28,7 +31,7 @@ for case, count in TEST_CASES:
 
         if cpp_ans.split() != py_ans.split():
             print("Py and cpp and are not same on test:")
-            with open(f"tests/{test_count}") as file:
+            with open(f"tests/{CURRENT_TASK}/{test_count}") as file:
                 print(file.read())
             print("py_ans:")
             print(py_ans)
@@ -36,7 +39,8 @@ for case, count in TEST_CASES:
             print(cpp_ans)
             sys.exit(1)
         else:
-            with open(f"tests/{test_count}.a", 'w') as file:
+            with open(f"tests/{CURRENT_TASK}/{test_count}.a", 'w') as file:
                 file.write(cpp_ans)
 
+        print(f"done {test_count} test")
         test_count += 1
