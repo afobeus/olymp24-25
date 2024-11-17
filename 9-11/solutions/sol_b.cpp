19ab220dbeaabd2bfa3b67181445b7ad0731a8d6
Artem <artem.dzichkanets@mail.ru>
2024-11-17 23:49:41 +0300

    task b changed

--- 9-11/solutions/sol_b.cpp
+++ 9-11/solutions/sol_b.cpp
@@ -6,21 +6,25 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    long long n;
-    cin >> n;
-    int path_len = 1;
-    long long max_number = n;
+    int t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        int path_len = 1;
+        long long max_number = n;
 
-    while (n != 1) {
-        ++path_len;
-        if (n % 2 == 0)
-            n /= 2;
-        else
-            n = 3 * n + 1;
-        max_number = max(max_number, n);
+        while (n != 1) {
+            ++path_len;
+            if (n % 2 == 0)
+                n /= 2;
+            else
+                n = 3 * n + 1;
+            max_number = max(max_number, n);
+        }
+
+        cout << path_len << ' ' << max_number << '\n';
     }
-
-    cout << path_len << ' ' << max_number;
 
     return 0;
 }
--- 9-11/solutions/sol_b.py
+++ 9-11/solutions/sol_b.py
@@ -1,13 +1,15 @@
-n = int(input())
-path_len = 1
-max_number = n
+t = int(input())
+numbers = map(int, input().split())
+for n in numbers:
+    path_len = 1
+    max_number = n
 
-while n != 1:
-    path_len += 1
-    if n % 2 == 0:
-        n //= 2
-    else:
-        n = 3 * n + 1
-    max_number = max(max_number, n)
+    while n != 1:
+        path_len += 1
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        max_number = max(max_number, n)
 
-print(path_len, max_number)
+    print(path_len, max_number)
