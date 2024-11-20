9e67d81deb75a8fa9df6eb554f86919298966d2a
Artem <artem.dzichkanets@mail.ru>
2024-11-20 12:19:24 +0300

    sol_d.cpp fix

--- 9-11/solutions/sol_d.cpp
+++ 9-11/solutions/sol_d.cpp
@@ -13,7 +13,7 @@ int main() {
     vector<vector<int>> allowed_before(10,
                                        vector<int> (10) = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9});
     int num;
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < m; ++i) {
         cin >> num;
         auto num_pos = allowed_before[num % 10].begin();
         while (*num_pos != num / 10)
