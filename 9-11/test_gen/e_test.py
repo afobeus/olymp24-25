cd94f93a0f335eb48992d331ee5c014a6a280e24
Artem <artem.dzichkanets@mail.ru>
2024-11-20 16:27:02 +0300

    e_test added

---
+++ 9-11/test_gen/e_test.py
@@ -0,0 +1,25 @@
+import random
+
+
+class TestCase:
+    def __init__(self, n_limits, q_limits, nums_limits):
+        self.n_limits, self.q_limits, self.nums_limits = n_limits, q_limits, nums_limits
+
+    def gen_case_common(self):
+        n = random.randint(*self.n_limits)
+        nums = [random.randint(*self.nums_limits) for _ in range(n)]
+        q = random.randint(*self.q_limits)
+        requests = [(random.randint(0, n - 1), random.randint(*self.nums_limits))]
+        return n, nums, q, requests
+
+
+def get_test_string(n, nums, q, requests):
+    return str(n) + '\n' + ' '.join(str(elem) for elem in nums) + '\n' + str(q) + '\n' + \
+        '\n'.join(f"{elem[0]} {elem[1]}" for elem in requests)
+
+
+TEST_CASES = [(TestCase((1, 10), (1, 10), (-10, 10)), 5),
+              (TestCase((10, 10 ** 2), (10, 10 ** 2), (-100, 100)), 10),
+              (TestCase((10 ** 2, 10 ** 4), (10 ** 2, 10 ** 4), (-10 ** 5, 10 ** 5)), 10),
+              (TestCase((10 ** 4, 10 ** 5), (10 ** 4, 10 ** 5), (-10 ** 9, 10 ** 9)), 5),
+              (TestCase((10 ** 5 - 1000, 10 ** 5), (10 ** 5 - 1000, 10 ** 5), (-10 ** 9, 10 ** 9)), 3)]
--- 9-11/test_gen/gen_test.py
+++ 9-11/test_gen/gen_test.py
@@ -3,8 +3,9 @@ import os
 import shutil
 from pathlib import Path
 
-CURRENT_TASK = 'd'
+CURRENT_TASK = 'e'
 COMPILER_PATH = "C:/mingw64/bin"
+pyton_solution = True
 
 if CURRENT_TASK == 'a':
     from a_test import TEST_CASES, get_test_string
@@ -15,14 +16,18 @@ if CURRENT_TASK == 'c':
 if CURRENT_TASK == 'd':
     from d_test import TEST_CASES, get_test_string
 if CURRENT_TASK == 'e':
-    from a_test import TEST_CASES, get_test_string
+    from e_test import TEST_CASES, get_test_string
+    pyton_solution = False
 
 
 test_count = 1
 path = Path(os.getcwd())
 parent_path = str(path.parent.absolute()).replace('\\', '/')
 os.chdir(parent_path)
-shutil.rmtree(f"tests/{CURRENT_TASK}")
+try:
+    shutil.rmtree(f"tests/{CURRENT_TASK}")
+except FileNotFoundError:
+    pass
 os.mkdir(f"tests/{CURRENT_TASK}")
 for case, count in TEST_CASES:
     for i in range(count):
@@ -30,14 +35,16 @@ for case, count in TEST_CASES:
             file.write(get_test_string(*case.gen_case_common()))
 
         os.system(f"{COMPILER_PATH}/task_{CURRENT_TASK}.exe < tests/{CURRENT_TASK}/{test_count} > temp_cpp.a")
-        os.system(f"python solutions/sol_{CURRENT_TASK}.py < tests/{CURRENT_TASK}/{test_count} > temp_py.a")
+        if pyton_solution:
+            os.system(f"python solutions/sol_{CURRENT_TASK}.py < tests/{CURRENT_TASK}/{test_count} > temp_py.a")
 
         with open("temp_cpp.a") as file:
             cpp_ans = file.read()
-        with open("temp_py.a") as file:
-            py_ans = file.read()
+        if pyton_solution:
+            with open("temp_py.a") as file:
+                py_ans = file.read()
 
-        if cpp_ans.split() != py_ans.split():
+        if pyton_solution and cpp_ans.split() != py_ans.split():
             print("Py and cpp and are not same on test:")
             with open(f"tests/{CURRENT_TASK}/{test_count}") as file:
                 print(file.read())
