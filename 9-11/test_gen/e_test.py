e087086e9681697172d6b68d5a1565f16ac18d9f
Artem <artem.dzichkanets@mail.ru>
2024-11-23 16:07:42 +0300

    9-11 e test gen fix

--- 9-11/test_gen/e_test.py
+++ 9-11/test_gen/e_test.py
@@ -9,13 +9,20 @@ class TestCase:
         n = random.randint(*self.n_limits)
         nums = [random.randint(*self.nums_limits) for _ in range(n)]
         q = random.randint(*self.q_limits)
-        requests = [(random.randint(0, n - 1), random.randint(*self.nums_limits))]
+        requests = []
+        for i in range(q):
+            requests.append([random.randint(1, 2)])
+            if requests[-1][0] == 1:
+                requests[-1].append(random.randint(0, n - 1))
+                requests[-1].append(random.randint(requests[-1][1], n - 1))
+            else:
+                requests[-1].extend([random.randint(0, n - 1), random.randint(*self.nums_limits)])
         return n, nums, q, requests
 
 
 def get_test_string(n, nums, q, requests):
     return str(n) + '\n' + ' '.join(str(elem) for elem in nums) + '\n' + str(q) + '\n' + \
-        '\n'.join(f"{elem[0]} {elem[1]}" for elem in requests)
+        '\n'.join(f"{elem[0]} {elem[1]} {elem[2]}" for elem in requests)
 
 
 TEST_CASES = [(TestCase((1, 10), (1, 10), (-10, 10)), 5),
