a75c58710b4bcaf6c80e2e8d1c446ed4866c71fb
Artem <artem.dzichkanets@mail.ru>
2024-11-23 10:56:44 +0300

    5-6 b_test added

---
+++ 5-6/test_gen/b_test.py
@@ -0,0 +1,21 @@
+import random
+
+
+class TestCase:
+    def __init__(self, n_limits):
+        self.n_limits = n_limits
+
+    def gen_case_common(self):
+        n = random.randint(*self.n_limits)
+        return n,
+
+
+def get_test_string(n):
+    return str(n)
+
+
+TEST_CASES = [(TestCase((1, 10)), 5),
+              (TestCase((10, 10 ** 3)), 10),
+              (TestCase((10 ** 3, 10 ** 9)), 10),
+              (TestCase((10 ** 9, 10 ** 18)), 10),
+              (TestCase((10 ** 18 - 10 ** 17, 10 ** 18)), 3)]
